e4feead8cbfd9ab28c51dc796bed13ad55f7bc3f
Yulia Lapshina <dymonyx13@gmail.com>
2024-04-03 02:36:05 +0300

    commit

--- proga_uni/sem_2/lab_3/task_3.py
+++ proga_uni/sem_2/lab_3/task_3.py
@@ -1,4 +1,14 @@
 import sqlite3
+
+musicians = [("Twenty One pilots", 36, "man"), ("GONE.Fludd", 24, "man"), ("Noize MC", 39, "man"),
+             ("Grimes", 27, "woman"), ("Turbowolf", 35, "man")]
+songs = [("ГОРОД", "remake к БРАТ 2", 2), ("Overcompenste", "Clancy album", 1),
+         ("No no no", "The Free Life album", 5), ("Вселенная бесконечна", "в клипе присутствует ИТМО", 3),
+         ("Genesis", "очень красивая песня", 4)]
+comments = [("NEW ERA YEAAAAA!1!", 2, 1), ("Посмотрю фильм только ради этой песни в исполнении флады", 1, 2),
+            ("МОЙ УНИК В КЛИПЕ НОЙЗА ЛОЛ!!", 4, 3), ("Раньше было лучше..", 5, 4),
+            ("hiatus =C", 3, 5)]
+
 try:
     connection = sqlite3.connect('music.db')
     cursor = connection.cursor()
@@ -12,13 +22,39 @@ try:
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     description TEXT,
-    id_Musicians INTEGER);""")
+    id_Musician INTEGER,
+    FOREIGN KEY (id_Musician) REFERENCES tMusician(id) ON DELETE CASCADE);""")
     cursor.execute("""CREATE TABLE IF NOT EXISTS tComments(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     text_of_comment TEXT,
     id_Songs INTEGER,
-    id_Musicians INTEGER); """)
-    cursor.execute("""SELECT id, nickname, age, sex, id.tSongs, name.tSongs, description.tSongs FROM tMusician, tSongs""")
-    cursor.execute("""SELECT id, nickname, age, sex, id.tComments, text_of_comment.tComments, id_Songs.tSongs FROM tMusican, tSongs, tComments """)
-    cursor.execute("""SELECT id, name, description, id_Musicians, id.tComments, text_of_comment.tComments, id.Songs.tComments FROM tSongs, tComments""")
+    id_Musician INTEGER,
+    FOREIGN KEY (id_Songs) REFERENCES tSongs(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_Musician) REFERENCES tMusician(id) ON DELETE CASCADE); """)
+    cursor.executemany('''INSERT INTO tMusician(nickname, age, sex) VALUES (?, ?, ?)''', musicians)
+    cursor.executemany('''INSERT INTO tSongs(name, description, id_Musician) VALUES (?, ?, ?)''', songs)
+    cursor.executemany('''INSERT INTO tComments(text_of_comment, id_Songs, id_Musician) VALUES (?, ?, ?)''', comments)
     connection.commit()
+
+    cursor.execute(
+        'SELECT tMusician.nickname, tSongs.name, tComments.text_of_comment FROM tMusician, tSongs, tComments WHERE tSongs.id_Musician = tMusician.id AND tComments.id_Musician = tMusician.id AND tComments.id_Songs = tSongs.id')
+    rows = cursor.fetchone()
+    print(rows)
+    rows = cursor.fetchmany(2)
+    print(rows)
+    record = cursor.fetchall()
+    print(record)
+
+    cursor.execute("DROP TABLE IF EXISTS tMusician")
+    cursor.execute("DROP TABLE IF EXISTS tSongs")
+    cursor.execute("DROP TABLE IF EXISTS tComments")
+    # закрыть курсор
+    cursor.close()
+except sqlite3.Error as error:
+    print("Ошибка при подключении к sqlite", error)
+finally:
+    # закрыть соединение с базой
+    if connection:
+        connection.commit()
+        connection.close()
+        print("Соединение с SQLite закрыто")
